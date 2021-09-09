796c2ab34cccbcc53be8876bc83c4b2bdce48d8f
Vincent Vatelot <vincent.vatelot@ext.adeo.com>
2021-09-09 12:50:53 +0200

    feat(sqlmodel): Use SQLModel instead of sqlalchemy model

--- db/database.py
+++ db/database.py
@@ -1,9 +1,13 @@
 from settings import DATABASE_URL
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import Session, SQLModel, create_engine
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
