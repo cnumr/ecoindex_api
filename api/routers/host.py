8a225fd76e6c66ed28a1922d78089ae4ba7fc3d2
Vincent Vatelot <vincent.vatelot@gmail.com>
2021-12-13 10:58:29 +0100

    Add date filter to host endpoint (#44)

--- api/routers/host.py
+++ api/routers/host.py
@@ -1,4 +1,5 @@
-from typing import List
+from datetime import date
+from typing import List, Optional
 
 from db.crud import get_host_list_db
 from db.database import get_session
@@ -20,8 +21,15 @@ router = APIRouter()
 )
 async def get_host_list(
     session: AsyncSession = Depends(get_session),
+    date_from: Optional[date] = Query(
+        None, description="Start date of the filter elements (example: 2020-01-01)"
+    ),
+    date_to: Optional[date] = Query(
+        None, description="End date of the filter elements  (example: 2020-01-01)"
+    ),
     q: str = Query(default=None, description="Filter by partial host name"),
 ) -> Page[str]:
-    hosts = await get_host_list_db(session=session, q=q)
-
+    hosts = await get_host_list_db(
+        session=session, date_from=date_from, date_to=date_to, q=q
+    )
     return paginate(hosts)
