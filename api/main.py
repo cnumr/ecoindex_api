1459ffe10db0fe9a4403a5169ef1cc5c1e00ec0e
Vincent Vatelot <vincent.vatelot@gmail.com>
2022-02-10 23:24:47 +0100

    feat(cors): Add CORS middleware (#75)

--- api/main.py
+++ api/main.py
@@ -1,7 +1,14 @@
 from db.engine import create_db_and_tables, is_database_online
 from fastapi.applications import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_health import health
 from fastapi_pagination.api import add_pagination
+from settings import (
+    CORS_ALLOWED_CREDENTIALS,
+    CORS_ALLOWED_HEADERS,
+    CORS_ALLOWED_METHODS,
+    CORS_ALLOWED_ORIGINS,
+)
 from starlette import status
 from starlette.requests import Request
 from starlette.responses import JSONResponse
@@ -15,6 +22,14 @@ app = FastAPI(
     title="Ecoindex API",
     version="1.0.0",
     description="Ecoindex API enables you to perform ecoindex analysis of given web pages",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=CORS_ALLOWED_CREDENTIALS,
+    allow_headers=CORS_ALLOWED_HEADERS,
+    allow_methods=CORS_ALLOWED_METHODS,
+    allow_origins=CORS_ALLOWED_ORIGINS,
 )
 
 app.include_router(router=ecoindex.router)
--- settings.py
+++ settings.py
@@ -3,5 +3,9 @@ from environ import Env
 env = Env()
 env.read_env()
 
+CORS_ALLOWED_CREDENTIALS = env.bool("CORS_ALLOWED_CREDENTIALS", default=True)
+CORS_ALLOWED_HEADERS = env("CORS_ALLOWED_HEADERS", cast=list, default=["*"])
+CORS_ALLOWED_METHODS = env("CORS_ALLOWED_METHODS", cast=list, default=["*"])
+CORS_ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS", cast=list, default=["*"])
 DAILY_LIMIT_PER_HOST = env("DAILY_LIMIT_PER_HOST", cast=int, default=0)
-DATABASE_URL = env("DATABASE_URL", cast=str, default="sqlite:///./sql_app.db")
+DATABASE_URL = env("DATABASE_URL", cast=str, default="sqlite+aiosqlite:///./sql_app.db")
