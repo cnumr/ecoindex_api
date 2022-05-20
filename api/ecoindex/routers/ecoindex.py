6663e4ef5dc4be6218b6fd229adcf68c0b3e51b0
Vincent Vatelot <vincent.vatelot@ik.me>
2022-05-20 16:58:36 +0200

    🐛 fix(validation): Fix integer validation for pagination

--- api/ecoindex/routers/ecoindex.py
+++ api/ecoindex/routers/ecoindex.py
@@ -121,9 +121,9 @@ async def get_ecoindex_analysis_list(
         None, description="End date of the filter elements  (example: 2020-01-01)"
     ),
     host: Optional[str] = Query(None, description="Host name you want to filter"),
-    page: Optional[int] = Query(1, description="Page number", gte=1),
+    page: Optional[int] = Query(1, description="Page number", ge=1),
     size: Optional[int] = Query(
-        50, description="Number of elements per page", gte=1, lte=100
+        50, description="Number of elements per page", ge=1, le=100
     ),
 ) -> PageApiEcoindexes:
     ecoindexes = await get_ecoindex_result_list_db(
