52c7db350b9e9d26a2def7d6ec6bf94d32d123fb
Vincent Vatelot <vincent.vatelot@ik.me>
2022-05-20 17:04:28 +0200

    🐛 fix(gunicorn): Set no worker timeout (#126)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn api.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+gunicorn api.main:app --timeout 0 --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
