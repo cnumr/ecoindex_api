eef228a44db812027ab9e5ca4fc6563613dcba6a
Vincent Vatelot <vincent.vatelot@gmail.com>
2022-09-05 16:57:23 +0200

    ✨ feat(429): Improve 429 response (#167)

--- tests/api/test_middleware.py
+++ tests/api/test_middleware.py
@@ -11,7 +11,7 @@ class TestMiddlewareApi:
     async def test_validate_analysis_request_over_quota(self, mocker):
         mocker.patch(
             "api.domain.ecoindex.repository.get_count_daily_request_per_host",
-            return_value=10,
+            return_value=5,
         )
 
         with pytest.raises(HTTPException) as exc:
@@ -19,13 +19,15 @@ class TestMiddlewareApi:
                 Response(),
                 get_session(),
                 WebPage(url="http://example.com"),
-                daily_limit_per_host=10,
+                daily_limit_per_host=5,
             )
         assert exc.value.status_code == status.HTTP_429_TOO_MANY_REQUESTS
         assert (
-            exc.value.detail
-            == "You have already reached the daily limit of 10 requests for host example.com today"
+            exc.value.detail["message"]
+            == "You have already reached the daily limit of 5 requests for host example.com today"
         )
+        assert exc.value.detail["daily_limit_per_host"] == 5
+        assert exc.value.detail["host"] == "example.com"
 
     async def test_validate_analysis_request_under_quota(self, mocker):
         mocker.patch(
@@ -37,5 +39,5 @@ class TestMiddlewareApi:
             Response(),
             get_session(),
             WebPage(url="http://example.com"),
-            daily_limit_per_host=10,
+            daily_limit_per_host=5,
         )
