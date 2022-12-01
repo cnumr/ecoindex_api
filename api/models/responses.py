673aa219ffcc5ea95e5e61f4365285d4b8966f07
Vincent Vatelot <vincent.vatelot@ik.me>
2022-12-01 14:50:41 +0100

    ♻️ refact(health): Improve Workers health response model

--- api/models/responses.py
+++ api/models/responses.py
@@ -1,8 +1,26 @@
-from typing import Dict, List
+from typing import List
 
+from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
+
+
+class WorkerHealth(BaseModel):
+    name: str = Field(default=..., title="Name of the worker")
+    healthy: bool = Field(
+        default=..., title="Set to true if the worker is healthy, else false"
+    )
+
+
+class WorkersHealth(BaseModel):
+    healthy: bool = Field(
+        default=...,
+        title="Set to true if all workers are healthy, false if one of them is down",
+    )
+    workers: List[WorkerHealth] = Field(
+        default=..., title="List of all current known workers"
+    )
 
 
 class ApiHealth(SQLModel):
     database: bool = Field(default=..., title="Status of database")
-    workers: List[Dict] = Field(default=..., title="Status of the queue task broker")
+    workers: WorkersHealth = Field(default=..., title="Status of the queue task broker")
