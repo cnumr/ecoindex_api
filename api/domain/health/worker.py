5d912bd8fa18b9b64d2c73b02d787dd8fd457754
Vincent Vatelot <vincent.vatelot@ik.me>
2022-12-01 15:10:53 +0100

    ♻️ refact(health): Improve Workers health response model (#238)

--- api/domain/health/worker.py
+++ api/domain/health/worker.py
@@ -1,5 +1,20 @@
+from api.models.responses import WorkerHealth, WorkersHealth
 from worker.tasks import app
 
 
 def is_worker_healthy():
-    return {"workers": app.control.ping()}
+    workers = []
+    workers_ping = app.control.ping()
+
+    for worker in workers_ping:
+        for name in worker:
+            workers.append(
+                WorkerHealth(name=name, healthy=True if "ok" in worker[name] else False)
+            )
+
+    result = WorkersHealth(
+        healthy=False if False in [w.healthy for w in workers] else True,
+        workers=workers,
+    )
+
+    return {"workers": result}
