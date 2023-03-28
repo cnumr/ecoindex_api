e99a8b63345b8715898f0f155e439866d65fa6d0
Vincent Vatelot <vincent.vatelot@ik.me>
2023-03-28 15:08:11 +0200

    🐛 fix(db): Update url field type to text to avoid error on long urls

--- db/alembic/versions/7eaafaa65b32_update_url_field_type_to_text.py
+++ db/alembic/versions/7eaafaa65b32_update_url_field_type_to_text.py
@@ -1,4 +1,4 @@
-"""Update URL field length
+"""Update URL field type to text
 
 Revision ID: 7eaafaa65b32
 Revises: e83263a5def4
@@ -8,7 +8,6 @@ Create Date: 2023-03-28 11:24:39.089063
 import sqlalchemy as sa
 from alembic import op
 
-# revision identifiers, used by Alembic.
 revision = "7eaafaa65b32"
 down_revision = "e83263a5def4"
 branch_labels = None
@@ -20,7 +19,7 @@ def upgrade() -> None:
         "apiecoindex",
         "url",
         existing_type=sa.String(length=2048),
-        type_=sa.String(length=4096),
+        type_=sa.Text(),
     )
 
 
@@ -28,6 +27,6 @@ def downgrade() -> None:
     op.alter_column(
         "apiecoindex",
         "url",
-        existing_type=sa.String(length=4096),
+        existing_type=sa.Text(),
         type_=sa.String(length=2048),
     )
